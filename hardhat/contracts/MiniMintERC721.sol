b37c1f80cc67af50d7e54a76dd62847cbb581f0f
nativealien <oskar@assarsson.nu>
2024-12-03 14:59:40 +0100

    ERC721 contract with tests added

--- hardhat/contracts/MiniMintERC721.sol
+++ hardhat/contracts/MiniMintERC721.sol
@@ -3,15 +3,84 @@ pragma solidity ^0.8.27;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MiniMintERC721 is ERC721 {
-    constructor() ERC721("MiniMint", "MTK") {}
-    /**
-     * @dev Allows anyone to mint an NFT.
-     * @param to The address of the recipient.
-     * @param tokenId The unique ID for the NFT.
-     */
-    function mint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
+contract MiniMintERC721 is ERC721, ERC721URIStorage, Ownable {
+    string private _contractURI; 
+    uint256 private _nextTokenId; 
+    uint256[] private _allMintedTokens; 
+
+    error NotOwner(address caller, uint256 tokenId);
+
+    event NFTMinted(address indexed to, uint256 indexed tokenId, string uri);
+    event MetadataUpdated(string oldURI, string newURI);
+    event NFTBurned(address indexed owner, uint256 indexed tokenId);
+
+    constructor(string memory contractMetadataURI) ERC721("MiniMint", "MTK") Ownable(msg.sender) {
+        _contractURI = contractMetadataURI;
+        _nextTokenId = 1; 
+    }
+
+    function setContractURI(string memory contractMetadataURI) public onlyOwner {
+        emit MetadataUpdated(_contractURI, contractMetadataURI);
+        _contractURI = contractMetadataURI;
+    }
+
+    function contractURI() public view returns (string memory) {
+        return _contractURI;
+    }
+
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _nextTokenId;
+        _nextTokenId++;
+        _allMintedTokens.push(tokenId); 
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+
+        emit NFTMinted(to, tokenId, uri);
+    }
+
+    function burn(uint256 tokenId) public {
+        if (ownerOf(tokenId) != msg.sender) {
+            revert NotOwner(msg.sender, tokenId); 
+        }
+        _burn(tokenId);
+
+        for (uint256 i = 0; i < _allMintedTokens.length; i++) {
+            if (_allMintedTokens[i] == tokenId) {
+                _allMintedTokens[i] = _allMintedTokens[_allMintedTokens.length - 1]; 
+                _allMintedTokens.pop(); 
+                break;
+            }
+        }
+        emit NFTBurned(msg.sender, tokenId);
+    }
+
+    function getNextTokenId() public view returns (uint256) {
+        return _nextTokenId;
+    }
+
+    function getAllMintedTokens() public view returns (uint256[] memory) {
+        return _allMintedTokens;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        ownerOf(tokenId);
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
+
