f38fad9bffa83121b131d20ffc8003cdec99e7b9
nativealien <oskar@assarsson.nu>
2024-12-11 11:06:40 +0100

    fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import { useOutletContext } from "react-router-dom"
+// import { useOutletContext } from "react-router-dom"
 import ConnectWeb3 from "../components/ConnectWeb3"
 
 const Home = () => {
-    const { local }: any = useOutletContext()
+    // const { local }: any = useOutletContext()
 
     return <div className="home">
         <h2>Home</h2>
